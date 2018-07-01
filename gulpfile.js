d7684aa8b9c2ebedf2123345ad528d0462a1c717
Juan Villela <juan@villela.co>
2018-07-01 12:50:32 -0700

    Migration and Gulpification!
    Migrating external blog to this site. So in preparation for that, I’ve made some major changes to also create a production workflow for the site.
    
    Added support for Gulp. Using Sass, Autoprefixer, Imagemin, Hash, and Del. All of which created an assets pipeline, cache busting and optimization of files.
    Added RSS template.
    Added Barefootjs and Chrome stylesheets and other assets.
    Migrated configuraitons from Clever Laziness.
    Migrated blog posts from Clever Laziness.
    Updated Gitignore.

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin     = require('gulp-imagemin'),
+    hash         = require('gulp-hash'),
+    del          = require('del');
+    
+
+// Compile SCSS files to CSS
+gulp.task('scss', function () {
+  del(['static/css/*'])
+  gulp.src('src/scss/main.scss')
+    .pipe(sass({outputStyle : 'compressed'}))
+    .pipe(autoprefixer({browsers : ['last 20 versions']}))
+    .pipe(hash())
+    .pipe(gulp.dest('static/css'))
+    //Create a hash map
+    .pipe(hash.manifest('hash.json')
+    //Put the map in the data directory
+    .pipe(gulp.dest('data/css')
+})
+
+// Hash img
+gulp.task('img', function () {
+  del(['static/img/*'])
+  gulp.src('src/img/*')
+    .pipe(hash())
+    .pipe(gulp.dest('static/img'))
+    .pipe(hash.manifest('hash.json')
+    .pipe(gulp.dest('data/img')
+})
+
+// Hash javascript
+gulp.task('js', function () {
+  del(['static/js/*'])
+  gulp.src('src/js/*')
+    .pipe(hash())
+    .pipe(gulp.dest('static/js'))
+    .pipe(hash.manifest('hash.json')
+    .pipe(gulp.dest('data/js')
+})
+
+// Watch asset folder for changes
+gulp.task('watch', ['scss', 'images', 'js'], function () {
+  gulp.watch('src/scss/*', ['scss'])
+  gulp.watch('src/js/*', ['js'])
+  gulp.watch('src/img/*', ['img'])
+});
+
+// Run Watch as default
+gulp.task('default', ['watch']);
