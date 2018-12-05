9e04ca7886583ff2b7c90d0ffd6dd66c9b711b7a
Juan <juan@villela.co>
2018-12-05 17:30:11 -0500

    Build updates
    Added node version to packaga.json.
    Added plumber to Gulpfile and all relevant Sass files to Watch.
    Updated PostCSS config to include know good parameters.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-const gulp         = require('gulp'),
-      rename       = require('gulp-rename'),
+const autoprefixer = require('autoprefixer'),
       concat       = require('gulp-concat-util'),
-      postcss      = require('gulp-postcss'),
-      autoprefixer = require('autoprefixer'),
       cssnano      = require('cssnano'),
+      gulp         = require('gulp'),
+      plumber      = require('gulp-plumber'),
+      postcss      = require('gulp-postcss')
+      rename       = require('gulp-rename'),
       sass         = require('gulp-sass');
-    
+      
 // Critical CSS
 gulp.task('critical', () => {
   const plugins = [
@@ -13,6 +14,7 @@ gulp.task('critical', () => {
     cssnano()
   ];
   return gulp.src(['assets/css/critical.scss'])
+  .pipe(plumber())
   .pipe(sass().on('error', sass.logError))
   .pipe(postcss(plugins))
   // wrap with style tags
@@ -28,8 +30,12 @@ gulp.task('critical', () => {
 });
 // Watch asset folder for changes
 gulp.task('watch', ['critical'], () => {
-  gulp.watch('assets/css/critical.scss', ['critical'])
-  gulp.watch('assets/css/reset.scss', ['critical'])
+  gulp.watch('assets/css/colors.scss', ['critical']);
+  gulp.watch('assets/css/critical.scss', ['critical']);
+  gulp.watch('assets/css/extends.scss', ['critical']);
+  gulp.watch('assets/css/fonts.scss', ['critical']);
+  gulp.watch('assets/css/mixins.scss', ['critical']);
+  gulp.watch('assets/css/reset.scss', ['critical']);
 });
 // Run Watch as default
 gulp.task('default', ['watch']);
