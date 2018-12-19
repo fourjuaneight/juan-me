11e812509770e0f8edb25edfe1b259375e8a64a2
Juan <juan@villela.co>
2018-12-19 15:29:20 -0500

    Gulp updated
    Added nvm config (8.14.0).
    Updated to Gulp 4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,47 @@
-const autoprefixer = require('autoprefixer'),
-      concat       = require('gulp-concat-util'),
-      cssnano      = require('cssnano'),
-      gulp         = require('gulp'),
-      plumber      = require('gulp-plumber'),
-      postcss      = require('gulp-postcss')
-      rename       = require('gulp-rename'),
-      sass         = require('gulp-sass');
-      
+const autoprefixer    = require('autoprefixer'),
+      concat          = require('gulp-concat-util'),
+      cssnano         = require('cssnano'),
+      gulp            = require('gulp'),
+      plumber         = require('gulp-plumber'),
+      postcss         = require('gulp-postcss'),
+      rename          = require('gulp-rename'),
+      sass            = require('gulp-sass');
+
 // Critical CSS
 gulp.task('critical', () => {
-  const plugins = [
-    autoprefixer({browsers: ['last 2 version']}),
-    cssnano()
-  ];
-  return gulp.src(['assets/css/critical.scss'])
-  .pipe(plumber())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(postcss(plugins))
-  // wrap with style tags
-  .pipe(concat.header('<style>'))
-  .pipe(concat.footer('</style>'))
-  // convert it to an include file
-  .pipe(rename({
-      basename: 'critical',
-      extname: '.html'
-    }))
-  // insert file
-  .pipe(gulp.dest('layouts/partials'));
+  const plugins = [autoprefixer({browsers: ['> 5%']}), cssnano()];
+  return (
+    gulp.src('assets/css/critical.scss')
+      .pipe(plumber())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss(plugins))
+      // wrap with style tags
+      .pipe(concat.header('<style>'))
+      .pipe(concat.footer('</style>'))
+      // convert it to an include file
+      .pipe(
+        rename({
+          basename: 'critical',
+          extname: '.html',
+        })
+      )
+      // insert file
+      .pipe(gulp.dest('layouts/partials'))
+  );
 });
+
 // Watch asset folder for changes
-gulp.task('watch', ['critical'], () => {
-  gulp.watch('assets/css/colors.scss', ['critical']);
-  gulp.watch('assets/css/critical.scss', ['critical']);
-  gulp.watch('assets/css/extends.scss', ['critical']);
-  gulp.watch('assets/css/fonts.scss', ['critical']);
-  gulp.watch('assets/css/mixins.scss', ['critical']);
-  gulp.watch('assets/css/reset.scss', ['critical']);
-});
+gulp.task('watch', gulp.series(['critical'], () => {
+  gulp.watch('assets/css/colors.scss', gulp.parallel(['critical']));
+  gulp.watch('assets/css/critical.scss', gulp.parallel(['critical']));
+  gulp.watch('assets/css/extends.scss', gulp.parallel(['critical']));
+  gulp.watch('assets/css/fonts.scss', gulp.parallel(['critical']));
+  gulp.watch('assets/css/mixins.scss', gulp.parallel(['critical']));
+  gulp.watch('assets/css/reset.scss', gulp.parallel(['critical']));
+}));
+
 // Run Watch as default
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series(['watch']));
+
 // Build
-gulp.task('build', ['critical']);
+gulp.task('build', gulp.series(['critical']));
